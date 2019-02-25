23ba1f83bbb5317f86ece1f4bba54a30bba21dc5
Manuel777a <36998380+Manuel777a@users.noreply.github.com>
2019-02-25 07:11:24 -0500

    Clase Tarea-TareaAdapter, XML de los cardview de Tareas
    hay que mirar bien lo del date no me he puesto a cacharrear con eso, falta distribucion grafica de tareasActivity (no tengo ni puta idea como organizar con ese coordinator)

---
+++ AplicacionTasksU/app/src/main/java/com/example/islas/calculadorappa/Tarea.java
@@ -0,0 +1,50 @@
+package com.example.islas.calculadorappa;
+
+import java.util.Date;
+
+public class Tarea {
+
+    private String nombre;
+    private String descripcion;
+    private Date fecha;
+    private double porcentaje;
+
+    public Tarea(String nombre, String descripcion, Date fecha, double porcentaje) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.porcentaje = porcentaje;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(double porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+}
